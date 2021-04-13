9338de7e2cacea381135b5d1c0f5b855c319972a
Diego-Paris <diego.paris@upr.edu>
2021-04-12 23:29:31 -0400

    added arrays

---
+++ chapter-4/4.1-arrays/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a [3]int // array of 3 integers, all zero valued
+
+	fmt.Println(a[0]) // print the first element
+
+	fmt.Println(a[len(a)-1]) // print the last element
+
+	// Print indices and values
+	for i, v := range a {
+		fmt.Printf("i = %v, v = %v\n", i, v)
+	}
+
+	// Print elements only
+	for _, v := range a {
+		fmt.Printf("v = %v\n", v)	
+	}
+
+	
+
+}
